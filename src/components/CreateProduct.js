da65f3445908b948bca820ace59a2acdec9fab7e
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-08-17 04:54:34 +0530

    add product page and sort products by price

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
 class CreateProduct extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      name: "",
+      description: "",
+      price: "",
+      rating: 0,
+      img: ""
+    }
+  }
+
+  handleInput = (e) =>{
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
   render() {
     return (
       <div>
-        Add new Product
+        <h1 className="page-heading">ADD PRODUCT</h1>
+        <form id="add-product-form">
+          <div className="input-group">
+            <label for="name">Name *</label>
+            <input type="text" id="name" className="custom-input" name="name" onChange={this.handleInput} value={this.state.name} />
+          </div>
+          <div className="input-group">
+            <label for="description">Description *</label>
+            <textarea id="description" className="custom-input" name="description" onChange={this.handleInput} value={this.state.description} rows="3" cols="30"></textarea>
+          </div>
+          <div className="input-group">
+            <label for="price">Price *</label>
+            <input type="number" id="price" className="custom-input" name="price" onChange={this.handleInput} value={this.state.price} />
+          </div>
+          <div className="input-group">
+            <label for="rating">Rating *</label>
+            <input type="range" min="0" max="5" step="0.1" id="rating" className="custom-input" name="rating" onChange={this.handleInput} value={this.state.rating} />
+          </div>
+          <div className="input-group">
+            <label for="img">URL *</label>
+            <input type="url" id="img" className="custom-input" name="img" onChange={this.handleInput} value={this.state.img} />
+          </div>
+          <div className="input-button-group">
+            <button type="button" className="btn-success">Add Product</button>
+            <button type="button" className="btn-danger">Reset</button>
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,14 @@ import Swal from 'sweetalert2';
 
 class Home extends Component {
   
+  constructor(props){
+    super(props);
+    this.state = {
+      sortEnabled: false,
+      sortedData: []
+    }
+  }
+
   handleClick = (e) => {
     console.log("Home ", e.target.id);
     const productId = e.target.id;
@@ -20,22 +28,50 @@ class Home extends Component {
     });
   }
 
+  toggleSort = (e) => {
+    console.log(e.target);
+    if(this.state.sortEnabled){
+      e.target.className="btn-success";
+      e.target.innerHTML="Sort By Price";
+      this.setState({
+        sortEnabled: false
+      })
+    }
+    else{
+      const { productsList } = this.props.state; 
+      let data = [...productsList];
+      data.sort((a, b) => a.price - b.price);
+      e.target.className="btn-danger";
+      e.target.innerHTML="Reset";
+      this.setState({
+        sortEnabled: true,
+        sortedData: data
+      })
+    }
+  }
+
   render() {
-    console.log('State ', this.props.state)
-    const { productsList } = this.props.state;
+    console.log('State ', this.props.state);
+    const productsList = this.state.sortEnabled ? this.state.sortedData : this.props.state.productsList;
+    // const { productsList } = this.props.state;
     return (
-      <div>
-        {
-          productsList.map((product, index) => (
-            <ProductCard
-              product={product} 
-              key={`product-${index}`}
-              dispatch={this.props.dispatch}
-              handleClick = {this.handleClick}
-              label ="Add to Cart"
-            /> 
-          ))
-        }
+      <div className="products-container">
+        <div>
+          {
+            productsList.map((product, index) => (
+              <ProductCard
+                product={product} 
+                key={`product-${index}`}
+                dispatch={this.props.dispatch}
+                handleClick = {this.handleClick}
+                label ="Add to Cart"
+              /> 
+            ))
+          }
+        </div>
+        <div className="products-sort">
+          <button type="button" className="btn-success" onClick={this.toggleSort}>Sort By Price</button>
+        </div>
       </div>
     );
   }
