8f6421809d38d0514e43bb751bbc252a962b0586
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-08-17 21:20:20 +0530

    Updates

---
+++ precache-manifest.fe875a5398e8cc01ff6b1a910ba718f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fe2c5ac62baa0763c65cd8b2de46af",
+    "url": "/ecommerce-app/index.html"
+  },
+  {
+    "revision": "74f4c57d1cc0cb960e23",
+    "url": "/ecommerce-app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f63f7b55a3829a61c677",
+    "url": "/ecommerce-app/static/css/main.333af674.chunk.css"
+  },
+  {
+    "revision": "74f4c57d1cc0cb960e23",
+    "url": "/ecommerce-app/static/js/2.82bb0e9f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecommerce-app/static/js/2.82bb0e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63f7b55a3829a61c677",
+    "url": "/ecommerce-app/static/js/main.979d0e06.chunk.js"
+  },
+  {
+    "revision": "2887ab20419561b80ff6",
+    "url": "/ecommerce-app/static/js/runtime-main.9463bbce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82d397b944c245fb5419601a30acacd8.js"
+  "/ecommerce-app/precache-manifest.fe875a5398e8cc01ff6b1a910ba718f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecommerce-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
